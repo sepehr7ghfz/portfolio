77015240577475467fae55241549b881c457e66e
sepehr7ghfz <spentax9@gmail.com>
2024-05-12 18:46:29 -0400

    init

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for portfolio project.
+"""portfolio URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,10 +14,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from home import views
+
+
+
+# django admin changes
+admin.site.site_header = "Login to Burhan"
+admin.site.site_title = "Welocom to DashBord"
+admin.site.index_title = "Welocom to Portal"
+
+
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('', include('home.urls'))
-
+    path('', views.home, name='home'),
+    path('project', views.project, name='project'),
+    path('#contact', views.contact, name='contact'),
+    # path('skills', views.skills, name='skills'),
+    # path('contact', views.contact, name='contact'),
 ]
